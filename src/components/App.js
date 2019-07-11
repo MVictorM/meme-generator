a6dc524eb919fb6e028a5fb00d175783d9058dfa
Victor Miranda <vmmelo1@gmail.com>
2019-07-11 17:58:30 -0300

    map function json exemplo

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,16 @@
 import React from 'react';
-import Joke from './Jokes';
+import Joke from './Joke';
+import jokesData from './jokesData'
 
 //arrow function
-const App = () =>
-    <div>
-        <Joke
-            question="O que é, o que é? Feito para andar e não anda."
-            answer="A rua."
-        />
-        <hr />
-        <Joke
-            question="O que é, o que é? Dá muitas voltas e não sai do lugar."
-            answer="O relógio."
-        />
-        <hr />
-        <Joke
-            question="O que é, o que é? Tem cabeça e tem dente, não é bicho e nem é gente."
-            answer="O alho."
-        />
-        <hr />
-        <Joke
-            question="O que é, o que é? Não se come, mas é bom para se comer."
-            answer="O talher."
-        />
-        <hr />
-        <Joke
-            question=""
-            answer="Era uma vez um pintinho que não tinha perna, foi ciscar e caiu"
-        />
+function App() {
+    //simulando um arquivo json
+    const jokeComponents = jokesData.map(joke =>
+        <Joke key={joke.id} question={joke.question} answer={joke.answer} />
+    );
+    return <div>
+        {jokeComponents}
     </div>;
+}
 
 export default App;
