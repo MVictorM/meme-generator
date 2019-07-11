83d985f0dc1974ed176080b261658b8dffd7cda8
Victor Miranda <vmmelo1@gmail.com>
2019-07-11 20:28:42 -0300

    state

--- src/components/ChildComponent.js
+++ src/components/ChildComponent.js
@@ -1,19 +1,18 @@
 import React, {Component} from 'react';
 
 //arrow function
-class App extends Component {
+class ChildComponent extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            name: 'Victor Miranda',
-            age: 24
+            answer: 'Yes'
         }
     }
 
     render() {
         return <div>
-            <h1>{this.state.name}</h1>
-            <h3>{this.state.age} years old</h3>
+            <h1>Is state important to know? {this.state.answer}</h1>
+            <ChildComponent answer={this.state.answer} />
         </div>;
     }
 }
