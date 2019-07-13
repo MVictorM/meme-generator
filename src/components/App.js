b211e89a7d22fa0dea403ede3f24bbdda772833e
Victor Miranda <vmmelo1@gmail.com>
2019-07-13 15:51:00 -0300

    todo app checking items

--- src/components/App.js
+++ src/components/App.js
@@ -8,11 +8,25 @@ class App extends Component {
         super(props);
         this.state = {
             todos: todosData,
-        }
+        };
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleChange(id) {
+        this.setState(prevState => {
+            //varre o objeto prevState e o que tiver o mesmo id que foi clicado, troca o completed
+            return prevState.todos.map(todo => {
+                if(todo.id === id) {
+                    todo.completed = !todo.completed;
+                }
+                return todo;
+            })
+        });
     }
 
     render() {
-        const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item}/>);
+        const todoItems = this.state.todos.map(item =>
+            <TodoItem key={item.id} item={item} handleChange={this.handleChange}/>);
         return <div className='todo-list'>
             {todoItems}
         </div>;
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,8 +2,12 @@ import React from 'react'
 
 function TodoItem(props) {
     return <div className='todo-item'>
-        <input type='checkbox' defaultChecked={props.item.completed}/>
-        <label>{props.item.text}</label>
+        <input
+            type='checkbox'
+            checked={props.item.completed}
+            onChange={() => props.handleChange(props.item.id)}
+        />
+        <label className={props.item.completed ? 'finalized' : ''}>{props.item.text}</label>
     </div>
 }
 
