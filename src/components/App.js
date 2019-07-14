6e1a06ea292ef2401e3953bc052fc9a7dea6e418
Victor Miranda <vmmelo1@gmail.com>
2019-07-14 14:35:34 -0300

    star wars api

--- src/components/App.js
+++ src/components/App.js
@@ -1,52 +1,34 @@
 import React, {Component} from 'react';
+//https://swapi.co/         --api de star wars
 
 //arrow function
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            character: {},
+            loading: true
         };
     }
 
     componentDidMount() {
-        //GET the data I need to correctly display
-        //The componentDidMount() method runs after the component output has been rendered to the DOM.
-        //This is a good place to set up a timer:
-        this.timerID = setInterval(
-            () => this.tick(),
-            1000
-        );
-    }
-
-    //quando o componente pai passa props, podemos verificar se mudou alguma coisa
-    componentDidUpdate(prevProps) {
-        // Typical usage (don't forget to compare props):
-        if (this.props.userID !== prevProps.userID) {
-            this.fetchData(this.props.userID);
-        }
-    }
-
-    //determina se o componente deve ser recarregado
-    //por padrão, react atualiza automaticamente quando tem alguma alteração
-    shouldComponentUpdate(nextProps, nextState) {
-        //return true if want it to update
-        //return false if not
-    }
-
-    getSnapshotBeforeUpdate(prevProps, prevState) {
-        //create a backup of the current way things are
-    }
-
-
-    componentWillUnmount() {
-        //teardown or cleanup your code before your component disappears
-        //exemple: remove event listener
-        clearInterval(this.timerID);
+        this.setState({
+            loading: true
+        });
+        fetch('https://swapi.co/api/people/1')
+            .then(response => response.json())
+            .then(data => {
+                //como não importa o que estava armazenado, não passa prevState como parametro
+                this.setState({
+                    character: data,
+                    loading: false
+                })
+            })
     }
 
     render() {
         return <div>
-
+            {this.state.loading ? 'Loading...' : this.state.character.name}
         </div>;
     }
 }
