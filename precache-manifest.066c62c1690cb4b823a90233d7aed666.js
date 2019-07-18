6bc55fadb79ded9ba9efd1e4cceb264ac2716ef6
Victor Miranda <victor.miranda@idealizza.com.br>
2019-07-18 16:19:04 -0300

    Updates

---
+++ precache-manifest.066c62c1690cb4b823a90233d7aed666.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79c59d209fcbe73a16e73d3872295468",
+    "url": "/meme-generator/index.html"
+  },
+  {
+    "revision": "835c2cf070826816d64c",
+    "url": "/meme-generator/static/css/main.bae504fa.chunk.css"
+  },
+  {
+    "revision": "0f19e2518a3a17b37f23",
+    "url": "/meme-generator/static/js/2.e2adb5f9.chunk.js"
+  },
+  {
+    "revision": "835c2cf070826816d64c",
+    "url": "/meme-generator/static/js/main.d19e006c.chunk.js"
+  },
+  {
+    "revision": "b8372d4f76d85c66a91a",
+    "url": "/meme-generator/static/js/runtime~main.0d1394e5.js"
+  }
+]);
